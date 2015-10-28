5a2ab2ece9cc0fdf04a87ed08ae978047b678ef9
z38 <z38@z38.invalid>
2015-10-28 20:55:45 +0100

    Fix BC break in BinaryXml

--- src/BinaryXml.php
+++ src/BinaryXml.php
@@ -2,11 +2,16 @@
 
 namespace CasperBiering\Dotnet;
 
+use CasperBiering\Dotnet\BinaryXml\Decoder;
+
 class BinaryXml
 {
-    public static function decode($content)
+    public static function decode($content, $indent = false)
     {
-        $decoder = new Decoder();
+        $decoder = new Decoder(array(
+            'indent' => $indent,
+            'dictionary' => 'str%d',
+        ));
 
         return $decoder->decode($content);
     }
